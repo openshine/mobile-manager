6e418c05e8632a0f66f104971b0a71852bb9b79e
Roberto Majadas <roberto.majadas@openshine.com>
2010-02-03 12:35:55 +0000

    Fix sierra 307

--- src/devices/MobileDeviceSierra.py
+++ src/devices/MobileDeviceSierra.py
@@ -74,9 +74,9 @@ class MobileDeviceSierra(MobileDevice):
                self.dev_props["usb_device.vendor_id"])
         
         if len(ports) >= 3 :
-            if dev == (0x6890,0x1199) and  len(ports) >= 5 :
-                self.set_property("conf-device", "/dev/%s" % ports[3])
-                self.set_property("data-device", "/dev/%s" % ports[4])
+            if dev == (0x68a3,0x1199) and  len(ports) >= 4 :
+                self.set_property("conf-device", "/dev/%s" % ports[2])
+                self.set_property("data-device", "/dev/%s" % ports[3])
             else:
                 self.set_property("conf-device", "/dev/%s" % ports[2])
                 self.set_property("data-device", "/dev/%s" % ports[0])
